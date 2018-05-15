dbfbbef669d7b0c8f8d26fa85cec884e5227fe79
amir-jafari <amir.h.jafari@okstate.edu>
2018-05-15 07:23:58 -0400

    Updated

--- Python-Math/Statistics/Exercise1.py
+++ Python-Math/Statistics/Exercise1.py
@@ -10,5 +10,6 @@
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Q-1: a) Create a function that generate random samples for each element of [0,1,2,3]
-#  that has probality of [0.1, 0.2, 0.3, 0.4].
+# Q-1: a) Create a function that generate 10000 random samples for each element of [0,1,2,3]
+#  that has probality of [0.1, 0.2, 0.3, 0.4]. In other words, the probability of happening 0
+# is 10 percent of 10,000 which is 1000 samples.
