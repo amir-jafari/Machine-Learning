d42f20236bb66f21264cce91f8e1b4a30dc59c01
amir-jafari <amir.h.jafari@okstate.edu>
2022-12-30 11:02:45 -0500

    Update

--- 04-Keras NN/Keras.py
+++ 04-Keras NN/Keras.py
@@ -17,6 +17,7 @@ from keras.optimizers import RMSprop
 batch_size = 128
 num_classes = 10
 epochs = 1
+
 # =============================================================
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
