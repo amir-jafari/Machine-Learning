6fd47a2fbd6784785f7e41fe9c828abe26f3b497
amir-jafari <amir.h.jafari@okstate.edu>
2022-12-31 00:33:50 -0500

    RBF Network Updated.

--- 07-RBF Network/RBF_NN.py
+++ 07-RBF Network/RBF_NN.py
@@ -1,9 +1,10 @@
-from utils import InitCentersRandom, RBFLayer,InitCentersKMeans
-import numpy as np, pandas as pd
+from utils import RBFLayer, InitCentersKMeans
+import numpy as np
+import pandas as pd
 from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.layers import Activation
-from keras.optimizers import RMSprop, SGD
+from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import OneHotEncoder
@@ -11,8 +12,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-data = pd.read_csv('olive.csv',header=None)
-datatrans=np.transpose(data)
+data = pd.read_csv('olive.csv', header=None)
+datatrans = np.transpose(data)
 print(datatrans[0].value_counts())
 datatrans[0].value_counts()[:].plot(kind='bar', alpha=0.5)
 plt.xlabel('\n Figure 1: Répartition selon classes \n', fontsize='17', horizontalalignment='center')
@@ -20,11 +21,11 @@ plt.tick_params(axis='x',  direction='out', length=10, width=3)
 plt.show()
 
 
-X=data.iloc[2:570,:].values
-y = data.iloc[0:1,:].values
+X = data.iloc[2:570, :].values
+y = data.iloc[0:1, :].values
 
-X=np.transpose(X)
-y=np.transpose(y)
+X = np.transpose(X)
+y = np.transpose(y)
 print('rotation ')
 print(X)
 print(y)
@@ -34,10 +35,10 @@ X = MinMaxScaler().fit_transform(X)
 ohe = OneHotEncoder()
 y = ohe.fit_transform(y).toarray()
 print('resulats de scalling')
-print(X,y)
+print(X, y)
 
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
 model = Sequential()
@@ -63,11 +64,11 @@ z_model = f"Z_model.h5"
 print(f"Save model to file {z_model} ... ", end="")
 model.save(z_model)
 
-newmodel1= load_model("Z_model.h5", custom_objects={'RBFLayer': RBFLayer})
+newmodel1 = load_model("Z_model.h5", custom_objects={'RBFLayer': RBFLayer})
 print("Evaluate on test data")
 results = newmodel1.evaluate(X_test, y_test, batch_size=32)
 print("test loss:", results[0])
-print("test accuracy:",results[1]*100,'%')
+print("test accuracy:", results[1]*100, '%')
 
 y_pred = newmodel1.predict(X_test)
 pred = list()
@@ -78,5 +79,5 @@ for i in range(len(y_test)):
     test.append(np.argmax(y_test[i]))
 
 
-a = accuracy_score(pred,test)
+a = accuracy_score(pred, test)
 print('Test Accuracy is:', a*100)
