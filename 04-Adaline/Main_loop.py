021d929c56e3ebe0bad5f2c54b0ccf3a9f924d48
amir-jafari <amir.h.jafari@okstate.edu>
2022-12-30 22:59:19 -0500

    All folders are restructured.

--- 04-Adaline/Main_loop.py
+++ 04-Adaline/Main_loop.py
@@ -1,34 +1,15 @@
 import numpy as np
-from perceptron import Perceptron
 import pandas as pd
+import matplotlib.pyplot as plt
+from plot_decision_regions import plot_decision_regions
 # -------------------------------------------------------------------------------------------------------
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 
 # -------------------------------------------------------------------------------------------------------
-import matplotlib.pyplot as plt
+
 y = df.iloc[0:100, 4].values
 y= np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
-plt.figure(1)
-plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
-plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
-plt.xlabel('petal length')
-plt.ylabel('sepal length')
-plt.legend(loc = 'upper left')
-ppn = Perceptron(eta=0.1, n_iter=10)
-ppn.fit(X, y)
-plt.figure(2)
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of misclassifications')
-
-# -------------------------------------------------------------------------------------------------------
-from plot_decision_regions import plot_decision_regions
-plt.figure(3)
-plot_decision_regions(X,y,classifier=ppn)
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc='upper left')
 
 # -------------------------------------------------------------------------------------------------------
 from AdalineGD import AdalineGD
