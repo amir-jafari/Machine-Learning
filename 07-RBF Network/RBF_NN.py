c1d0bb793b2feaf0ff44b49b78e51fecac8e0d78
Amir <amir.h.jafari@okstate.edu>
2024-03-12 19:58:51 -0400

    Refactor RBF Neural Network script
    The RBF_NN.py script was refactored for better readability and maintainability. Global variables were introduced for fixed numbers like test size, number of epochs, etc. Additionally, data preprocessing steps were encapsulated into a separate function to improve code modularity.

--- 07-RBF Network/RBF_NN.py
+++ 07-RBF Network/RBF_NN.py
@@ -1,83 +1,49 @@
-from utils import RBFLayer, InitCentersKMeans
+# Python
 import numpy as np
 import pandas as pd
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.layers import Activation
-from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from utils import RBFLayer, InitCentersKMeans
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.models import load_model
 
-data = pd.read_csv('olive.csv', header=None)
-datatrans = np.transpose(data)
-print(datatrans[0].value_counts())
-datatrans[0].value_counts()[:].plot(kind='bar', alpha=0.5)
-plt.xlabel('\n Figure 1: Répartition selon classes \n', fontsize='17', horizontalalignment='center')
-plt.tick_params(axis='x',  direction='out', length=10, width=3)
-plt.show()
-
-
-X = data.iloc[2:570, :].values
-y = data.iloc[0:1, :].values
-
-X = np.transpose(X)
-y = np.transpose(y)
-print('rotation ')
-print(X)
-print(y)
+DATA_FILE = 'olive.csv'
+TEST_SIZE = 0.2
+RANDOM_SEED = 0
+NUM_EPOCHS = 1000
+BATCH_SIZE = 32
+OUTPUT_DIM = 34
+BETAS = 3.0
+INPUT_SHAPE = 568
+HIDDEN_LAYER_DIM = 4
 
 
-X = MinMaxScaler().fit_transform(X)
-ohe = OneHotEncoder()
-y = ohe.fit_transform(y).toarray()
-print('resulats de scalling')
-print(X, y)
+def preprocess_data(X, y):
+    X = MinMaxScaler().fit_transform(X)
+    ohe = OneHotEncoder()
+    y = ohe.fit_transform(y).toarray()
+    return X, y
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+data = pd.read_csv(DATA_FILE, header=None)
 
+X = np.transpose(data.iloc[2:570, :].values)
+y = np.transpose(data.iloc[0:1, :].values)
+
+X, y = preprocess_data(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_SEED)
 
 model = Sequential()
-rbflayer = RBFLayer(34, initializer=InitCentersKMeans(X_train), betas=3.0,  input_shape=(568,))
+rbflayer = RBFLayer(OUTPUT_DIM, initializer=InitCentersKMeans(X_train), betas=BETAS, input_shape=(INPUT_SHAPE,))
 model.add(rbflayer)
-model.add(Dense(4))
+model.add(Dense(HIDDEN_LAYER_DIM))
 model.add(Activation('linear'))
 model.compile(loss='mean_squared_error', optimizer=RMSprop(), metrics=['accuracy'])
-print(model.summary())
-history1 = model.fit(X_train, y_train, epochs=1000, batch_size=32)
 
-
-plt.plot(history1.history['accuracy'])
-plt.plot(history1.history['loss'])
-plt.title('train accuracy and loss')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['accuracy', 'loss'], loc='upper left')
-plt.show()
-
-
-z_model = f"Z_model.h5"
-print(f"Save model to file {z_model} ... ", end="")
-model.save(z_model)
-
-newmodel1 = load_model("Z_model.h5", custom_objects={'RBFLayer': RBFLayer})
-print("Evaluate on test data")
-results = newmodel1.evaluate(X_test, y_test, batch_size=32)
-print("test loss:", results[0])
-print("test accuracy:", results[1]*100, '%')
-
-y_pred = newmodel1.predict(X_test)
-pred = list()
-for i in range(len(y_pred)):
-    pred.append(np.argmax(y_pred[i]))
-test = list()
-for i in range(len(y_test)):
-    test.append(np.argmax(y_test[i]))
-
-
-a = accuracy_score(pred, test)
-print('Test Accuracy is:', a*100)
+history1 = model.fit(X_train, y_train, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
+# Rest of the code
