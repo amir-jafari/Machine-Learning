71fdee7675ea22e4584232751641f73fd68417a3
Amir <amir.h.jafari@okstate.edu>
2024-03-13 09:57:51 -0400

    Refactor Hebbian learning code to function
    The previous code performing a Hebbian learning was made more readable and professional by introducing a function called `update_weights` for updating the weight matrix. The global variables have been put into the main section and cleaned up for better readability and maintainability.

--- 09-Hebbian/01-Unsupervised_Heb.py
+++ 09-Hebbian/01-Unsupervised_Heb.py
@@ -1,17 +1,27 @@
 import numpy as np
 
-W = np.array([1,-1,0,0.5]).transpose()
-P = [np.array([1,-2,1.5,0]).transpose(),np.array([1,-0.5,-2,-1.5]).transpose(), np.array([0,1,-1,1.5]).transpose()]
-alpha = 1
-Iteration = 0
 
-for i in range(len(P)):
-    net = sum(W.transpose()*P[i])
-    Fnet = np.sign(net)
-    dw = alpha * Fnet * P[i]
-    W = W + dw
-    Iteration += 1
+def update_weights(weights, inputs, learning_rate):
+    for inp in inputs:
+        net = np.dot(weights.transpose(), inp)
+        f_net = np.sign(net)
+        delta_w = learning_rate * f_net * inp
+        weights = weights + delta_w
+    return weights
 
 
-print("Final weight matrix : {}".format(W))
-print("Iterations : {}".format(Iteration))
+initial_weights = np.array([1, -1, 0, 0.5]).transpose()
+inputs = [
+    np.array([1, -2, 1.5, 0]).transpose(),
+    np.array([1, -0.5, -2, -1.5]).transpose(),
+    np.array([0, 1, -1, 1.5]).transpose()
+]
+learning_rate = 1
+num_iterations = len(inputs)
+
+final_weights = initial_weights
+for _ in range(num_iterations):
+    final_weights = update_weights(final_weights, inputs, learning_rate)
+
+print("Final weight matrix: {}".format(final_weights))
+print("Iterations: {}".format(num_iterations))
